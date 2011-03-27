8c4180ada050f64e7f4e998ed8178dab35b3a4fc
Ben Godfrey <bpgodfrey@gmail.com>
2011-03-27 23:37:47 +0100

    Add skeleton for integration tests using Common Test

---
+++ test/rolf_SUITE.erl
@@ -0,0 +1,64 @@
+%% @doc Common test suite for Rolf.
+%% @author Ben Godfrey <ben@ben2.com> [http://aftnn.org/]
+%% @copyright 2011 Ben Godfrey
+%% @version 1.0.0
+%%
+%% Rolf - a monitoring and graphing tool like Munin or collectd.
+%% Copyright (C) 2011 Ben Godfrey.
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+-module(rolf_SUITE).
+-compile(export_all).
+
+-include("ct.hrl").
+
+%% ===================================================================
+%% Common Test callbacks
+%% ===================================================================
+
+% Specify a list of all unit test functions
+all() -> [test1].
+
+% required, but can just return Config. this is a suite level setup function.
+init_per_suite(Config) ->
+    % do custom per suite setup here
+    Config.
+
+% required, but can just return Config. this is a suite level tear down function.
+end_per_suite(Config) ->
+    % do custom per suite cleanup here
+    Config.
+
+% optional, can do function level setup for all functions,
+% or for individual functions by matching on TestCase.
+init_per_testcase(TestCase, Config) ->
+    % do custom test case setup here
+    Config.
+
+% optional, can do function level tear down for all functions,
+% or for individual functions by matching on TestCase.
+end_per_testcase(TestCase, Config) ->
+    % do custom test case cleanup here
+    Config.
+
+%% ===================================================================
+%% Test cases
+%% ===================================================================
+
+test1(Config) ->
+    % write standard erlang code to test whatever you want
+    % use pattern matching to specify expected return values
+    ok.
+
