fc7fe8fae3fa474b862fb67ffb6d5b9a61abb587
Ben Godfrey <bpgodfrey@gmail.com>
2011-04-03 19:24:56 +0100

    Generate graphs of both test metrics

--- graph.sh
+++ graph.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 now=`date +%s`
 start=`expr $now - 3600`
-rrdtool graph graph.png -s $start -S 10 -w 800 -h 600 -l 0 -u 1.5 \
+rrdtool graph loadtime.png -s $start -S 10 -w 800 -h 600 -l 0 -u 1.5 \
         -c BACK#191919 -c CANVAS#191919 -c SHADEA#191919 -c SHADEB#191919 -c FONT#ffffff \
         'DEF:loadtime=priv/data/rolf@josie/loadtime.rrd:loadtime:AVERAGE' \
         'AREA:loadtime#0091ff:loadtime'
-open graph.png
+rrdtool graph freespace.png -s $start -S 300 -w 800 -h 600 -l 0 -u 100 \
+        -c BACK#191919 -c CANVAS#191919 -c SHADEA#191919 -c SHADEB#191919 -c FONT#ffffff \
+        'DEF:freespace=priv/data/rolf@josie/disk.rrd:freespace:AVERAGE' \
+        'AREA:freespace#0091ff:freespace'
+open freespace.png loadtime.png
