797975d2b80bc481dc83183fe1073454b9306fb6
Ben Godfrey <bpgodfrey@gmail.com>
2011-04-03 19:16:34 +0100

    More bling test graph script

--- graph.sh
+++ graph.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
-rrdtool graph graph.png --start 1300650000 --step 10 \
-        'DEF:loadtime=priv/data/frank@josie/loadtime.rrd:loadtime:AVERAGE' \
-        'AREA:loadtime#ff0000:loadtime'
+#!/bin/bash
+now=`date +%s`
+start=`expr $now - 3600`
+rrdtool graph graph.png -s $start -S 10 -w 800 -h 600 -l 0 -u 1.5 \
+        -c BACK#191919 -c CANVAS#191919 -c SHADEA#191919 -c SHADEB#191919 -c FONT#ffffff \
+        'DEF:loadtime=priv/data/rolf@josie/loadtime.rrd:loadtime:AVERAGE' \
+        'AREA:loadtime#0091ff:loadtime'
 open graph.png
